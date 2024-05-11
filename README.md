80c7ab16fc129edc30e47c86cb0ac0afb5417c3b
dfhao12 <dfhao12@163.com>
2024-05-11 12:40:55 +0800

    demo配置验证改善

---
+++ README.md
@@ -0,0 +1,5 @@
+# icode
+
+## 介绍
+* ispringboot 项目为脚手架项目，聚合了所有demo 的功能
+* demo-xxx 项目为 验证某一功能点的测试项目
--- demo-swagger/src/main/java/com/dfhao/swagger/test/TestController.java
+++ demo-swagger/src/main/java/com/dfhao/swagger/test/TestController.java
@@ -2,7 +2,6 @@ package com.dfhao.swagger.test;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "测试类名称")
 public class TestController {
 
-    @GetMapping("/get-str")
-    @Operation(description = "查询接口")
+    @GetMapping("/")
+    @Operation(description = "get请求接口")
     @Parameter(name = "str", description = "字符串", required = true)
     public String getStr(@RequestParam("str") String str) {
+        System.out.println("1");
         return str;
     }
 
-    @PostMapping("/info")
-    @Operation(summary = "获取信息", description = "查询接口")
-    public String info(@RequestParam("str") Person  person) {
-        return "";
+    @PostMapping("/")
+    @Operation(summary = "post请求接口", description = "接口描述")
+    public void info(@RequestBody Person person) {
+        System.out.println("2");
     }
 }
