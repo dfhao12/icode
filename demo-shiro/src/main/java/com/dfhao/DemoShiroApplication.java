aa5991f31c9348565c959b53c0bcdc61b767ea4c
dfhao <dfhao12@163.com>
2024-07-13 15:58:05 +0800

    调整工程

--- demo-shiro/src/main/java/com/dfhao/DemoShiroApplication.java
+++ demo-shiro/src/main/java/com/dfhao/DemoShiroApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShiroApplication {
+public class DemoShiroApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShiroApplication.class, args);
+        SpringApplication.run(DemoShiroApplication.class, args);
     }
 
 }
