bb12b6fc7925eac0ce871ae27d0b96244d030ac7
dfhao12 <dfhao12@163.com>
2024-07-23 13:32:37 +0800

    添加逻辑流验证

--- iadmin/src/main/java/com/dfhao/admin/user/UserController.java
+++ iadmin/src/main/java/com/dfhao/admin/user/UserController.java
@@ -1,14 +1,24 @@
 package com.dfhao.admin.user;
 
+import com.yomahub.liteflow.builder.el.LiteFlowChainELBuilder;
+import com.yomahub.liteflow.core.FlowExecutor;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
+import javax.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 @Tag(name = "测试类名称")
 public class UserController {
+    @Resource
+    private FlowExecutor flowExecutor;
 
     @GetMapping("/")
     @Operation(summary = "")
@@ -16,6 +26,14 @@ public class UserController {
     public String getStr(@RequestParam("str") String str) {
         System.out.println("1");
         return str;
+    }
+
+    @GetMapping("/liteFlow")
+    @Operation(summary = "逻辑流验证")
+    public void liteFlow() {
+        LiteFlowChainELBuilder.createChain().setChainId("test11").setEL("THEN(a, b)").build();
+
+        flowExecutor.execute2Resp("test11");
     }
 
     @PostMapping("/")
--- iadmin/src/main/java/com/dfhao/admin/user/UserServiceImpl.java
+++ iadmin/src/main/java/com/dfhao/admin/user/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.dfhao.admin.user;
 
 import com.dfhao.base.BaseServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl extends BaseServiceImpl<UserMapper, User> implements IUserService {
 
 }
